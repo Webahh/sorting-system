df6f51513d82b25d622d451012f459941b39aac5
Webahh <weber-fcv@outlook.de>
2024-05-30 14:19:43 +0200

    Analog & Branch

---
+++ Src/Board/STM32L100-Discovery/Analog.h
@@ -0,0 +1,23 @@
+#idndef ANALOG_H
+#define ANALOG_H
+
+#include "EmbSysLib.h"
+#include "config.h"
+
+class Analog{
+
+	public:
+	virtual int getValue() = 0;
+	virtual ~Analog();
+	
+	
+	
+	protected:
+	Adc_Mcu adc(timer);
+	
+	
+	private:
+
+};
+
+#endif
